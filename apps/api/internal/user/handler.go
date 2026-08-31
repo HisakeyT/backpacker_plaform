9356393153874d9c66c400f1a6a93f9470cb786b
hisakeyT <110322156+HisakeyT@users.noreply.github.com>
2026-09-01 05:37:19 +0900

    fix: email required

--- apps/api/internal/user/handler.go
+++ apps/api/internal/user/handler.go
@@ -19,7 +19,7 @@ func NewHandler(useCase *UseCase) *Handler {
 
 type RegisterRequest struct {
 	Nickname             string `json:"nickname" binding:"required"`
-	Email                string `json:"email"`
+	Email                string `json:"email" binding:"required"`
 	Password             string `json:"password" binding:"required"`
 	PasswordConfirmation string `json:"password_confirmation" binding:"required"`
 }
